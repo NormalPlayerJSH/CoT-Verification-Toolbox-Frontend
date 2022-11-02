0d7503fd61f9e8d55669942ee9091605f60ef65f
parksw1230 <parksw123098@gmail.com>
2022-11-02 19:11:00 +0900

    Result에 Graph, Solution 추가

--- src/Pages/Result.tsx
+++ src/Pages/Result.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Graph from '../Components/Graph.jsx';
+import Solution from '../Components/Solution';
 import MakeRate from '../Components/MakeRate';
 
 function Result() {
   return (
     <div className="grid grid-cols-3 w-full h-full p-4 gap-2">
-      <div className="col-start-1 border">풀이방법 자리 1</div>
-      <div className="col-start-2 border">풀이방법 자리 2</div>
+      <div className="col-start-1 border">
+        <Graph />
+      </div>
+      <div className="col-start-2 border">
+        <Solution />
+      </div>
       <div className="col-start-3 border p-4">
         <MakeRate />
       </div>
