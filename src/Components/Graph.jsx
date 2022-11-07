f760c152fa3eb9dbfca474cb4184e95d11d75eba
parksw1230 <parksw123098@gmail.com>
2022-11-07 20:22:25 +0900

    그래프 동적 생성

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import { useEffect, useRef, useState } from 'react';
 import Tree from 'react-d3-tree';
-
-const orgChart = {
-
-    name: 'Step1',
-    attributes: {
-        solution: "The Statue of Freedom is in Washington D.C., which is about 300 miles from the Statue of Liberty.",
-        link: "https://www.naver.com",
-    },
-    children: [
-        {
-            name: 'Step2',
-            attributes: {
-                solution: 'None',
-            },
-            children: [
-                {
-                    name: 'Step3',
-                    attributes: {
-                        solution: 'None'
-                    },
-                },
-            ],
-        },
-    ],
-}
+import data from "../db/db.json";
 
 const renderNodeWithCustomEvents = ({
     nodeDatum,
@@ -40,19 +16,63 @@ const renderNodeWithCustomEvents = ({
     </g>
 );
 
+
 function Graph(props) {
     const ref = useRef(null);
+    const [orgChart, setOrgChart] = useState({});
     const [height, setHeight] = useState(0);
     const [width, setWidth] = useState(0);
+    const chartData = JSON.parse(JSON.stringify(data));
+    const tempChart = {
+        name: '',
+        attributes: {
+            subquery: '',
+            solution: '',
+            link: '',
+        },
+        children: [],
+    };
+    const makeChart = (stepCount, chartData, target) => {
+        if (stepCount == 0) {
+            target.name = "Step" + String(stepCount + 1);
+            target.attributes.subquery = chartData["nodeList"][stepCount]["subQuery"];
+            target.attributes.solution = chartData["nodeList"][stepCount]["solution"];
+            target.attributes.link = chartData["nodeList"][stepCount]["url"];
+            makeChart(stepCount + 1, chartData, target)
+            return;
+        }
+        if (stepCount < chartData["stepCount"]) {
+            const temp = {
+                name: '',
+                attributes: {
+                    subquery: '',
+                    solution: '',
+                    link: '',
+                },
+                children: [],
+            };
+            temp.name = "Step" + String(stepCount + 1);
+            temp.attributes.subquery = chartData["nodeList"][stepCount]["subQuery"];
+            temp.attributes.solution = chartData["nodeList"][stepCount]["solution"];
+            temp.attributes.link = chartData["nodeList"][stepCount]["url"];
+            target.children.push(temp);
+            makeChart(stepCount + 1, chartData, target.children[0]);
+            return;
+        }
+        return;
+    };
+    makeChart(0, chartData, tempChart);
     useEffect(() => {
         setHeight(ref.current.offsetHeight);
         setWidth(ref.current.offsetWidth);
+        setOrgChart(tempChart);
     }, []);
     const handleNodeHover = (nodeDatum) => {
-        props.setSolution(nodeDatum.attributes.solution)
+        const text = nodeDatum.attributes.subquery + "\n" + nodeDatum.attributes.solution;
+        props.setSolution(text);
     };
     const handleNodeClick = (nodeDatum) => {
-        window.open(nodeDatum.attributes.link)
+        window.open(nodeDatum.attributes.link);
     };
     return (
         <div id="treeWrapper" ref={ref} style={{ margin: '0', padding: '0', width: '100%', height: '100%' }}>
